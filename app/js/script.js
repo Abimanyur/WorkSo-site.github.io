18e15d2d21a471d664b9f693a2f18e41372f6859
Abimanyu <111635811+Abimanyur@users.noreply.github.com>
2024-01-27 16:53:38 +0530

    hero page completed

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,7 @@
+const navToggle = document.getElementById("menu-icon")
+const navbar =document.querySelector("nav")
+
+navToggle.addEventListener("click", function(){
+    navbar.classList.toggle("link-show")
+})
 console.log("hello world")
